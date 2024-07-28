7d350596d7aee49eba42ff9cecd5ed68dc964a4e
Mito Youhei <mitoyouhei@proton.me>
2024-07-28 14:15:12 +0800

    refactor code enable logic delete, never really delete

--- models/Folder.js
+++ models/Folder.js
@@ -11,6 +11,56 @@ const folderSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
   createdAt: { type: Date, default: Date.now },
   updateAt: { type: Date, default: Date.now },
+  deletedAt: { type: Date, default: null },
+  isDeleted: { type: Boolean, default: false },
 });
+
+folderSchema.statics.getRoot = async function (userId) {
+  const folder = await this.findOne({ userId, parentId: null });
+  if (folder) return folder;
+  const root = new this({
+    name: "root",
+    parentId: null,
+    userId,
+  });
+  await root.save();
+  return root;
+};
+folderSchema.statics.create = async function (name, userId, parentId, isFile) {
+  if (!parentId) {
+    parentId = (await this.getRoot(userId))._id;
+  }
+
+  const folder = new this({
+    name: name,
+    parentId: parentId,
+    userId,
+    isFile,
+  });
+  await folder.save();
+  return folder;
+};
+folderSchema.statics.getFolders = async function (userId) {
+  let folders = await this.find({
+    userId,
+    $or: [
+      { isDeleted: false },
+      { isDeleted: { $exists: false } },
+      { isDeleted: null },
+    ],
+  });
+  if (folders.length === 0) {
+    folders.push(await this.getRoot(userId));
+  }
+  return folders;
+};
+
+folderSchema.statics.deleteFolder = async function (userId, folderId) {
+  await this.findOneAndUpdate(
+    { _id: folderId, userId },
+    { isDeleted: true, deletedAt: new Date() }
+  );
+};
+
 
 module.exports = mongoose.model("Folder", folderSchema);
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
-// server/models/User.js
+const { v4: uuid } = require("uuid");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const AuthToken = require("./AuthToken");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -22,6 +23,25 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.comparePassword = function (candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
+};
+
+userSchema.statics.register = async function (username, password) {
+  const user = new this({ username, password });
+  await user.save();
+  const token = await AuthToken.generate(user);
+  return { user, token };
+};
+userSchema.statics.login = async function (username, password) {
+  const user = await this.findOne({ username });
+  if (!user) {
+    throw new Error("Incorrect username.");
+  }
+  const isMatch = await user.comparePassword(password);
+  if (!isMatch) {
+    throw new Error("Incorrect password.");
+  }
+  const token = await AuthToken.generate(user);
+  return { user, token };
 };
 
 module.exports = mongoose.model("User", userSchema);
